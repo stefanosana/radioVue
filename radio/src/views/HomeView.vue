2b93d37c03f1544a53255d8db8b80a9627076d25
gabriarcu <38346968+gabriarcu@users.noreply.github.com>
2024-04-30 09:18:06 +0000

    init

---
+++ radio/src/views/HomeView.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-container>
+    <h1>Homepage</h1>
+  </v-container>
+</template>
+
+<script>
+
+
+export default {
+  name: 'HomeView',
+  data() {
+    return {
+      radios: [],
+
+    }
+  },
+  methods: {
+    getRadios() {
+      fetch('https://nl1.api.radio-browser.info/json/stations/search?limit=100&countrycode=IT&hidebroken=true&order=clickcount&reverse=true')
+        .then(response => response.json())
+        .then(data => {
+          this.radios = data;
+          console.log(data);
+        });
+
+    },
+  },
+  created() {
+    this.getRadios();
+  },
+}
+</script>
